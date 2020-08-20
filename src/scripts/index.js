f5c08905a6a6815dec9c07a3f2c4df005552c07e
Hugo <albrecht@pop-os.localdomain>
2020-08-20 18:18:44 -0300

    refatorando o arquivo js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,33 +2,19 @@ let inputElement = document.querySelector('#container .user');
 let buttonElement = document.querySelector('#container .search');
 let listElement = document.querySelector('#container .repo-list');
 
-let repositories = [];
-
 const searchInfo = () => {    
     let inputText = inputElement.value;
     
+    listElement.innerHTML = '';
+
     renderLoading();
 
     axios.get(`https://api.github.com/users/${inputText}/repos`)
         .then(response => {
-
+            const repositories = response.data;
             listElement.innerHTML= '';
 
-            repositories.push(...response.data);
-            repositories.forEach(item => {
-                
-                let repoElementLi = document.createElement('li');
-                let repoElementA = document.createElement('a');
-                let repoAText = document.createTextNode(`${item.full_name}`);
-                    
-                    repoElementA.appendChild(repoAText);
-                    repoElementA.setAttribute('href', `${item.html_url}`);
-                    repoElementA.setAttribute('target', '_blank')
-                    
-                    repoElementLi.appendChild(repoElementA);
-
-                    listElement.appendChild(repoElementLi);
-            });
+            renderInfo(repositories);
         })
         .catch(error => {
             console.warn(error);
@@ -43,4 +29,21 @@ const renderLoading = () => {
         loadingElement = document.createTextNode('Loading...');
         
     listElement.appendChild(loadingElement);
-}
+};
+
+const renderInfo = (repositories) => {
+    repositories.forEach(item => {
+                
+        let repoElementLi = document.createElement('li');
+        let repoElementA = document.createElement('a');
+        let repoAText = document.createTextNode(`${item.full_name}`);
+            
+            repoElementA.appendChild(repoAText);
+            repoElementA.setAttribute('href', `${item.html_url}`);
+            repoElementA.setAttribute('target', '_blank')
+            
+            repoElementLi.appendChild(repoElementA);
+
+            listElement.appendChild(repoElementLi);
+    });
+};
